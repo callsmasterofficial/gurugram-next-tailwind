5b9b4f8cb97287ed2a595f7f63163b0099c84d7e
Anoj Singh <anoj.callsmaster@gmail.com>
2022-07-12 10:34:03 +0530

    Swiper replaced by React-Slick

--- components/Header_smallSlider.jsx
+++ components/Header_smallSlider.jsx
@@ -1,66 +1,99 @@
-import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Autoplay } from "swiper";
-import "swiper/css";
-import "swiper/css/autoplay";
-import "swiper/css/navigation";
 
-export default function Header_smallSlider() {
-  return (
-    <div className="w-[80%]  m-auto   3xl:w-[70%] 4xl:w-[60%] 5xl:w-[50%] 6xl:w-[45%]  3xl:m-auto ">
-      <Swiper
-        // install Swiper modules
-        modules={[Navigation, Autoplay]}
-        slidesPerView={1}
-        navigation={{
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        }}
-        // navigation
-        autoplay={true}
-        // loop={true}
-        breakpoints={{
-          640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          // 768: {
-          //   slidesPerView: 3,
-          //   spaceBetween: 2,
-          // },
-          1024: {
-            slidesPerView: 3,
-            spaceBetween: 40,
-          },
-        }}
-        // loop={true}
-      >
-        {" "}
-        <div className="swiper-button-next"></div>
-        <div className="swiper-button-prev"></div>
+import React, { Component } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import LeftArrow from "../components/icons/LeftArrow"
+import RightArrow from "../components/icons/RightArrow"
+
+export default class PreviousNextMethods extends Component {
+  constructor(props) {
+    super(props);
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+  }
+  next() {
+    this.slider.slickNext();
+  }
+  previous() {
+    this.slider.slickPrev();
+  }
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      autoplay: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      adaptiveHeight: true,
+      draggable: true,
+      responsive: [
+                    {
+                        breakpoint: 1024,
+                        settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                        }
+                    },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        initialSlide: 2
+                        }
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        }
+                    }
+                    ]
+    };
+    return (
+      // <div className="overflow-hidden">
+    <div className="w-[80%] relative  m-auto 3xl:w-[70%] 4xl:w-[60%] 5xl:w-[50%] 6xl:w-[45%]  3xl:m-auto overflow-hidden">
+        <h2 className="w-5 h-5"></h2>
+        <div className="">
+          <button className="mx-1 p-[3px] transition-all absolute z-10 left-0 top-[50%] bg-black rounded-full text-white" onClick={this.previous}>
+            <LeftArrow color="white" width="1.5rem" height="1.5rem" />
+          </button>
+          <button className="mx-1 p-[3px] transition-all absolute z-10 right-0 top-[50%] bg-black rounded-full text-white" onClick={this.next}>
+            <RightArrow color="white" width="1.5rem" height="1.5rem" />
+          </button>
+        </div>
+        <Slider ref={(c) => (this.slider = c)} {...settings}>
         {Header_smallSliderData.map((data) => {
-          return (
-            <SwiperSlide key={data.id} className="smallSizeSlide mt-2">
-              <div className="smallSlider p-4 lg:p-0 lg:grid lg:grid-cols-2">
-                <div className="smallSizeImgbox sm:float-left sm:mx-2 lg:mx-0 lg:col-span-1">
-                  <img src={data.img_src} alt="slider items" className="w-[180px] h-[87px] sm:w-[110px] sm:h-[67px]" />
-                </div>
-                <div className="mx-auto lg:col-span-1 ">
-                  <p className="text-center sm:text-start text-[14px] lg:text-[14px]">{data.title}</p>
-                </div>
-              </div>
-            </SwiperSlide>
-          );
+           return (
+          <div key={data.id} className="relative">
+            <div className="p-1">
+              <img className="object-contain imageFix" src="https://images.unsplash.com/photo-1557992260-ec58e38d363c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" alt="newFormat" />
+            </div>
+            <div className="absolute bottom-2 left-2 text-white ml-6 mr-6 titleFix">
+              <p>{data.title}</p>
+            </div>
+          </div>
+           );
         })}
-      </Swiper>
-    </div>
-  );
+          
+        </Slider>
+        
+      </div>
+    );
+  }
 }
+
+
 
 const Header_smallSliderData = [
   {
     id: "1",
-    img_src: "./assets/images/post-1.jpg",
+    img_src: "./assets/images/gallery-post/1.png",
     title: "The Home Decoration Document: Photo...",
     sub_title: "People have been infected in USA...",
   },
@@ -108,90 +141,90 @@ const Header_smallSliderData = [
   },
 ];
 
-/*
+// /*
 
 
 
-Updated till 7th July 2022, 04:22 pm
-<Swiper
-        // install Swiper modules
-        modules={[Navigation, Autoplay]}
-        // spaceBetween={50}
-        slidesPerView={1}
-        navigation={{
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        }}
-        // navigation
-        autoplay={true}
-        // loop={true}
-        breakpoints={{
-          640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 3,
-            spaceBetween: 40,
-          },
-          1024: {
-            slidesPerView: 4,
-            spaceBetween: 50,
-          },
-        }}
-        // loop={true}
-      >
-        {" "}
-        <div className="swiper-button-next"></div>
-        <div className="swiper-button-prev"></div>
-        {Header_smallSliderData.map((data) => {
-          return (
-            <SwiperSlide key={data.id} className="mt-2">
-              <div className="smallSlider p-4">
-                <div className=" sm:float-left md:mx-2">
-                  <img src={data.img_src} alt="slider items" className="w-[110px] h-[67px]" />
-                </div>
-                <div className="mx-auto">
-                  <p className="text-[9px] lg:text-[14px]">{data.title}</p>
-                </div>
-              </div>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
+// Updated till 7th July 2022, 04:22 pm
+// <Swiper
+//         // install Swiper modules
+//         modules={[Navigation, Autoplay]}
+//         // spaceBetween={50}
+//         slidesPerView={1}
+//         navigation={{
+//           nextEl: ".swiper-button-next",
+//           prevEl: ".swiper-button-prev",
+//         }}
+//         // navigation
+//         autoplay={true}
+//         // loop={true}
+//         breakpoints={{
+//           640: {
+//             slidesPerView: 2,
+//             spaceBetween: 20,
+//           },
+//           768: {
+//             slidesPerView: 3,
+//             spaceBetween: 40,
+//           },
+//           1024: {
+//             slidesPerView: 4,
+//             spaceBetween: 50,
+//           },
+//         }}
+//         // loop={true}
+//       >
+//         {" "}
+//         <div className="swiper-button-next"></div>
+//         <div className="swiper-button-prev"></div>
+//         {Header_smallSliderData.map((data) => {
+//           return (
+//             <SwiperSlide key={data.id} className="mt-2">
+//               <div className="smallSlider p-4">
+//                 <div className=" sm:float-left md:mx-2">
+//                   <img src={data.img_src} alt="slider items" className="w-[110px] h-[67px]" />
+//                 </div>
+//                 <div className="mx-auto">
+//                   <p className="text-[9px] lg:text-[14px]">{data.title}</p>
+//                 </div>
+//               </div>
+//             </SwiperSlide>
+//           );
+//         })}
+//       </Swiper>
 
 
-Updated till 7th July 2022, 12:22 pm
+// Updated till 7th July 2022, 12:22 pm
 
-<div className="w-[80%]  m-auto   3xl:w-[70%] 4xl:w-[60%] 5xl:w-[50%] 6xl:w-[45%]  3xl:m-auto ">
-      <Swiper
-        // install Swiper modules
-        modules={[Navigation, Autoplay]}
-        // spaceBetween={50}
-        slidesPerView={4}
-        navigation={{
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        }}
-        // navigation
-        autoplay={true}
-        loop={true}
-      >
-        {" "}
-        <div className="swiper-button-next"></div>
-        <div className="swiper-button-prev"></div>
-        {Header_smallSliderData.map((data) => {
-          return (
-            <SwiperSlide key={data.id} className="border-4">
-              <div className="smallSlider bg-red-200">
-                <div>
-                  <img src={data.img_src} alt="slider items" />
-                </div>
-              </div>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
-    </div>
+// <div className="w-[80%]  m-auto   3xl:w-[70%] 4xl:w-[60%] 5xl:w-[50%] 6xl:w-[45%]  3xl:m-auto ">
+//       <Swiper
+//         // install Swiper modules
+//         modules={[Navigation, Autoplay]}
+//         // spaceBetween={50}
+//         slidesPerView={4}
+//         navigation={{
+//           nextEl: ".swiper-button-next",
+//           prevEl: ".swiper-button-prev",
+//         }}
+//         // navigation
+//         autoplay={true}
+//         loop={true}
+//       >
+//         {" "}
+//         <div className="swiper-button-next"></div>
+//         <div className="swiper-button-prev"></div>
+//         {Header_smallSliderData.map((data) => {
+//           return (
+//             <SwiperSlide key={data.id} className="border-4">
+//               <div className="smallSlider bg-red-200">
+//                 <div>
+//                   <img src={data.img_src} alt="slider items" />
+//                 </div>
+//               </div>
+//             </SwiperSlide>
+//           );
+//         })}
+//       </Swiper>
+//     </div>
 
-*/
+// */
--- components/TrendingNewsCarousel.jsx
+++ components/TrendingNewsCarousel.jsx
@@ -1,68 +1,167 @@
-import { data } from "autoprefixer";
-import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from "swiper";
-import "swiper/css";
-import "swiper/css/autoplay";
 
-export default function TrendingNewsCarousel() {
-  return (
-    <>
-      <Swiper
-        // install Swiper modules
-        modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
-        spaceBetween={50}
-        slidesPerView={1}
-        navigation
-        autoplay
-        pagination={{
-          el: ".swiper-pagination",
-          clickable: true,
-        }}
-        scrollbar={{ draggable: true }}
-        breakpoints={{
-          // when window width is >= 640px
-          460: {
-            width: 640,
-            slidesPerView: 1,
-          },
-          // when window width is >= 768px
-          768: {
-            width: 768,
-            slidesPerView: 2,
-            spaceBetween: 50,
-          },
-        }}
-      >
+
+
+
+import React, { Component } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import LeftArrow from "../components/icons/LeftArrow"
+import RightArrow from "../components/icons/RightArrow"
+
+export default class PreviousNextMethods extends Component {
+  constructor(props) {
+    super(props);
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+  }
+  next() {
+    this.slider.slickNext();
+  }
+  previous() {
+    this.slider.slickPrev();
+  }
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      autoplay: true,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      adaptiveHeight: true,
+      draggable: true,
+      responsive: [
+                    {
+                        breakpoint: 1024,
+                        settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        initialSlide: 2
+                        }
+                    },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        initialSlide: 1
+                        }
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        }
+                    }
+                    ]
+    };
+    return (
+      // <div className="overflow-hidden">
+    <div className="w-[80%] relative  m-auto 3xl:w-[70%] 4xl:w-[60%] 5xl:w-[50%] 6xl:w-[45%]  3xl:m-auto overflow-hidden">
+        <h2 className="w-5 h-5"></h2>
+        <div className="">
+          <button className="mx-1 p-[3px] transition-all absolute z-10 left-0 top-[50%] bg-black rounded-full text-white" onClick={this.previous}>
+            <LeftArrow color="white" width="1.5rem" height="1.5rem" />
+          </button>
+          <button className="mx-1 p-[3px] transition-all absolute z-10 right-0 top-[50%] bg-black rounded-full text-white" onClick={this.next}>
+            <RightArrow color="white" width="1.5rem" height="1.5rem" />
+          </button>
+        </div>
+        <Slider ref={(c) => (this.slider = c)} {...settings}>
         {TrendingNewsCarouselData.map((data) => {
-          return (
-            <SwiperSlide key={data.id}>
-              <div key={data.id} className="trending-post lg:mx-auto">
-                <div className="trendingNewsCarousel  w-[350px] mx-auto sm:w-[400px] sm:items-center">
-                  <img src={data.img_src} alt="trending" className="" />
-                </div>
-                <div className="p-5 flex flex-col justify-center">
-                  <div className="inline text-lg font-bold">
-                    <a href="#" className="text-[#1da1f2]">
-                      {data.category}
-                    </a>
-                    <span>{" / "}</span>
-                    <span>{data.date}</span>
-                  </div>
-
-                  <h3 className="">
-                    <a href="#">{data.title}</a>
-                  </h3>
-                  <p className="">{data.sub_title}</p>
-                </div>
-              </div>
-            </SwiperSlide>
-          );
+           return (
+          <div key={data.id} className="relative">
+            <div className="p-1">
+              <img className="object-cover TrendingImageFix" src={data.img_src} alt="newFormat" />
+            </div>
+            <div className="absolute bottom-2 left-2 text-white ml-6 mr-6 titleFix">
+              <p>{data.title}</p>
+            </div>
+          </div>
+           );
         })}
-      </Swiper>
-    </>
-  );
+          
+        </Slider>
+        
+      </div>
+    );
+  }
 }
+
+
+
+
+
+
+
+
+// import { data } from "autoprefixer";
+// import React from "react";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from "swiper";
+// import "swiper/css";
+// import "swiper/css/autoplay";
+
+// export default function TrendingNewsCarousel() {
+//   return (
+//     <>
+//       <Swiper
+//         // install Swiper modules
+//         modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+//         spaceBetween={50}
+//         slidesPerView={1}
+//         navigation
+//         autoplay
+//         pagination={{
+//           el: ".swiper-pagination",
+//           clickable: true,
+//         }}
+//         scrollbar={{ draggable: true }}
+//         breakpoints={{
+//           // when window width is >= 640px
+//           460: {
+//             width: 640,
+//             slidesPerView: 1,
+//           },
+//           // when window width is >= 768px
+//           768: {
+//             width: 768,
+//             slidesPerView: 2,
+//             spaceBetween: 50,
+//           },
+//         }}
+//       >
+//         {TrendingNewsCarouselData.map((data) => {
+//           return (
+//             <SwiperSlide key={data.id}>
+//               <div key={data.id} className="trending-post lg:mx-auto">
+//                 <div className="trendingNewsCarousel  w-[350px] mx-auto sm:w-[400px] sm:items-center">
+//                   <img src={data.img_src} alt="trending" className="" />
+//                 </div>
+//                 <div className="p-5 flex flex-col justify-center">
+//                   <div className="inline text-lg font-bold">
+//                     <a href="#" className="text-[#1da1f2]">
+//                       {data.category}
+//                     </a>
+//                     <span>{" / "}</span>
+//                     <span>{data.date}</span>
+//                   </div>
+
+//                   <h3 className="">
+//                     <a href="#">{data.title}</a>
+//                   </h3>
+//                   <p className="">{data.sub_title}</p>
+//                 </div>
+//               </div>
+//             </SwiperSlide>
+//           );
+//         })}
+//       </Swiper>
+//     </>
+//   );
+// }
 
 const TrendingNewsCarouselData = [
   {
