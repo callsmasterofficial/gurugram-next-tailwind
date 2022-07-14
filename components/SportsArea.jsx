096d96cee57ae431dc10f12337dd6b07c9a08a39
Anoj Singh <anoj.callsmaster@gmail.com>
2022-07-14 18:24:13 +0530

    Ninety percent Mobile Changes

--- components/SportsArea.jsx
+++ components/SportsArea.jsx
@@ -6,7 +6,7 @@ import SportsAreaCarousel from "./SportsAreaCarousel";
 
 function SportsArea() {
   return (
-    <div className="w-[80%]  m-auto   3xl:w-[70%] 4xl:w-[60%] 5xl:w-[50%] 6xl:w-[45%]  3xl:m-auto ">
+    <div className="w-[80%] ninetyPercentWidth m-auto   3xl:w-[70%] 4xl:w-[60%] 5xl:w-[50%] 6xl:w-[45%]  3xl:m-auto ">
 
 <div className="  m-auto text-xl font-semibold  mb-4 border-t-2 border-b-2 p-2 mt-24">
     <h1>Sports News</h1>
--- components/TrendingNewsArea.jsx
+++ components/TrendingNewsArea.jsx
@@ -6,7 +6,7 @@ import TrendingSinglePost from "./TrendingSinglePost";
 
 export default function TrendingNewsArea() {
   return (
-    <div className="w-[80%]  m-auto   3xl:w-[70%] 4xl:w-[60%] 5xl:w-[50%] 6xl:w-[45%]  3xl:m-auto ">
+    <div className="w-[80%] ninetyPercentWidth m-auto   3xl:w-[70%] 4xl:w-[60%] 5xl:w-[50%] 6xl:w-[45%]  3xl:m-auto ">
       <div className=" text-xl font-semibold border-t-2 border-b-2 p-2 mt-24">
         <h1>Trending News</h1>
       </div>
