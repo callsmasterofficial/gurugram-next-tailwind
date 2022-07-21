837de476d9d93ad85bae2f9b935f49d760498677
Anoj Singh <anoj.callsmaster@gmail.com>
2022-07-21 15:49:44 +0530

    weather component construct

--- components/NewTWeather.jsx
+++ components/NewTWeather.jsx
@@ -1,37 +1,15 @@
 import React from "react";
 import DateTime from "./DateTime";
 import { Chat } from "./icons";
+import indiaGateImg from "../public/assets/images/indiaGateImg.svg"
 
 export default function NewTWeather() {
   return (
     <>
-      <div className="border-2 p-2 relative">
-        {/* Image via global CSS */}
-        <div className=" weatherbgImage h-52"></div>
-        <div className="absolute bottom">
-          {/* Place */}
-          <div className="w-[100%] text-[28px] font-bold text-center mb-4">
-            <p>Delhi</p>
-            <div className="text-[8px]">
-              <DateTime />
-            </div>
-          </div>
-
-          {/* temp & other details  */}
-          <div className="flex flex-wrap items-center">
-            <div className="mx-auto flex flex-col items-center p-2">
-              <div>
-                <Chat />
-              </div>
-              <p className="text-[36px]">21<sup>°</sup>c</p>
-            </div>
-            <div className="mx-auto p-2">
-              <p>39°-27°</p>
-              <p>56%</p>
-              <p>5.14 km/h</p>
-            </div>
-          </div>
-
+      <div className="border-2 p-2">
+        <div className="w-[100%] h-[100%]">
+          <img src="../public/assets/images/indiaGateImg.svg" alt="delhi weather" />
+          <p>this is it...</p>
         </div>
       </div>
     </>
